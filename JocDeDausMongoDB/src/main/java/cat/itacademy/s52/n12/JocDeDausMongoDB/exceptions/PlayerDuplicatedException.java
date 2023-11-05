56e1b16a2f302f5c658add12286311d6218c11f1
Joan Carles <57914114+leibaral@users.noreply.github.com>
2023-11-05 12:51:03 +0100

    Delete JocDeDausMongoDB directory

--- JocDeDausMongoDB/src/main/java/cat/itacademy/s52/n12/JocDeDausMongoDB/exceptions/PlayerDuplicatedException.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.s52.n12.JocDeDausMongoDB.exceptions;
-
-public class PlayerDuplicatedException extends RuntimeException {
-    public PlayerDuplicatedException(String message){
-        super(message);
-    }
-
-}
--- JocDeDausMongoDB/src/main/java/cat/itacademy/s52/n12/JocDeDausMongoDB/exceptions/PlayerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cat.itacademy.s52.n12.JocDeDausMongoDB.exceptions;
-
-public class PlayerNotFoundException extends RuntimeException{
-    public PlayerNotFoundException(String message){
-        super(message);
-    }
-}
